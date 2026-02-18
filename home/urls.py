2fc8096987a09bde0bdce6ab140d2e37d0085a25
Amit <ydaamit992@gmail.com>
2026-02-18 18:14:31 +0545

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from home.views import home, test_views, school_list
+from home.views import home, test_views, school_list, school_create2
 
 urlpatterns = [
     path('',home, name="home"),
     path('home2', test_views, name="test"),
-    path('school', school_list, name = "school-list")
+    path('school', school_list, name = "school-list"),
+    path('school-create', school_create2, name = "school-create")
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from home.models import School
+from home.forms import SchoolForm
 
 # Create your views here.
 def home(request):
     print("home func")
     return HttpResponse("Hello this is from home func")
+
 
 def test_views(request):
     return JsonResponse({
@@ -14,6 +15,38 @@ def test_views(request):
         "address":"nepal"
     },safe=False)
 
+
 def school_list(request):
     school = School.objects.all()
     return render(request, 'school/school.html', {"school":school})
+
+
+def school_create(request):
+    if request.method == "POST":
+        data = request.POST
+        School.objects.create(
+            name = data['name'],
+            school_unique_code = data['unique_code'],
+            address = data['address'],
+        )
+        return redirect('/home/school')
+    return render(request, 'school/create.html',)
+
+
+def school_create2(request):
+
+    if request.method == "POST":
+        data = request.POST
+        form = SchoolForm(data=data)
+        if form.is_valid():
+            form.save()
+            return redirect('/home/school')
+        else:
+            print(form.errors)
+    form = SchoolForm()
+    context = {
+        "form":form
+    }
+    if request.method == "POST":
+        print("........",form.errors)
+    return render(request, 'school/create2.html',context=context)
